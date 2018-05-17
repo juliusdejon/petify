f75c9e2a36ddc9d1589c4437774269a6d2f306d9
Julius Dejon <dejonjulz@gmail.com>
2018-05-17 13:00:44 +0800

    Setup Redux and middlewares

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
@@ -8,19 +10,22 @@ import Navbar from './components/layout/Navbar';
 
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <Router>
-          <div>
-            <Navbar />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </div>
-        </Router>
-      </div>
+      <Provider store={store}>
+        <div>
+          <Router>
+            <div>
+              <Navbar />
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+          </Router>
+        </div>
+      </Provider>
     );
   }
 }
