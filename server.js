5d17da78fb088f552b67f9008d0a140cfe4b07d9
Julius Dejon <dejonjulz@gmail.com>
2018-06-16 03:15:38 +0800

    Production Settings

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 
@@ -32,7 +33,15 @@ require('./config/passport')(passport);
 // Use Routes
 app.use('/api/users', users);
 
+// Server static assets if in production 
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 // Run Server
 const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
